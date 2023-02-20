b86b22b8f24da3a44219a88cb3257b5962764300
Kycoon04 <jomaval4@gmail.com>
2023-02-19 19:18:51 -0600

    se agrega mi proyecto

---
+++ src/App.js
@@ -0,0 +1,281 @@
+import './App.css';
+function App() {
+  return (
+    <div className="App"> 
+      <header className="App-header">
+      <div className='glow2'></div>
+        <img className='background' src='./background.gif'/>
+        <div className='glow'>
+        <img className='banner'src='./gif.gif'/>
+        <h1 className='Kycoon'>Kycoon</h1>
+        </div>
+        <ul className="Menu">
+          <li><a className="opcion1" href='#inicioxs'>About me</a></li>
+          <li><a className="opcion1" href='#inicioxc'>Services</a></li>
+          <li><a className="opcion1" href='#inicioxx'>Contact</a></li>
+          <li className="nav1"><a className="opcion2" href='https://discord.gg/J9Cp994EXU'></a></li>
+          <li className="nav1"><a className="opcion3" href='https://github.com/Kycoon04'></a></li>
+          <li className="nav1"><a className="opcion4" href='https://twitter.com/Kycoon_'></a></li>
+          <li className="nav1"><a className="opcion5" href='https://www.reddit.com/user/Kycoon_'></a></li>
+          
+        </ul>
+      </header>
+      <img className='background2' src='./texture.gif'/>
+      <p id='inicioxs'>.</p>
+      <div id='About_me'>
+          <h1 className='AboutTitle'>About me</h1>
+          <p className='AboutParrafo'>hello, i'm Kycoon I've been study and working in software developer, with more than 1 years' experience, at present I work with diferent programing languages among them: C++, java, html, css etc...  I offer excellent support in English, and take
+           a professional and solution-oriented approach to every project. I am confident in my ability to deliver high-quality results using a variety of different technologies, Highly 
+           creative and multi-talented Web Developer, and I love to face challenges with different types of problems those are related to programming. </p>
+          <img className='pokemon' src='./pokemon.gif'/>
+      </div>
+      <div id='About_me1'>
+      <div id="phone">
+      <div id="main-wrapper" class="fancy-scrollbar">
+      <div id="main">
+      <div id="content">
+        <div id="links-container">
+          <a href="https://youtu.be/DLzxrzFCyOs">Private</a>
+          <a href="https://mail.google.com">Gmail</a>
+          <a href="https://apps.microsoft.com/store/apps">Store</a>
+        </div>
+        <div id="header-container">
+          <img id="logo" src="https://www.google.com/logos/2023/boba/rc3/cta.gif" />
+          <button type="button" id="user-menu-button">
+            <img src="https://assets.codepen.io/1468070/Hyperplexed+Menu+Image.png" alt="" />
+          </button>
+        </div>
+
+        <div id="apps-container">
+          <a href="https://codepen.io">
+            <i class="fa-brands fa-codepen"></i>
+            <div class="label">
+              <span class="name">CodePen</span>
+              <span class="url">codepen.io</span>
+            </div>
+          </a>
+          <a href="https://youtube.com">
+            <i class="fa-brands fa-youtube"></i>
+            <div class="label">
+              <span class="name">YouTube</span>
+              <span class="url">youtube.com</span>
+            </div>
+          </a>
+          <a href="https://spotify.com">
+            <i class="fa-brands fa-spotify"></i>
+            <div class="label">
+              <span class="name">Spotify</span>
+              <span class="url">spotify.com</span>
+            </div>
+          </a>
+          <a href="https://twitter.com">
+            <i class="fa-brands fa-twitter"></i>
+            <div class="label">
+              <span class="name">Twitter</span>
+              <span class="url">twitter.com</span>
+            </div>
+          </a>
+          <a href="https://airbnb.com">
+            <i class="fa-brands fa-airbnb"></i>
+            <div class="label">
+              <span class="name">Airbnb</span>
+              <span class="url">airbnb.com</span>
+            </div>
+          </a>
+          <a href="https://wikipedia.com">
+            <i class="fa-brands fa-wikipedia-w"></i>
+            <div class="label">
+              <span class="name">Wikipedia</span>
+              <span class="url">wikipedia.com</span>
+            </div>
+          </a>
+          <a href="https://github.com">
+            <i class="fa-brands fa-github"></i>
+            <div class="label">
+              <span class="name">Github</span>
+              <span class="url">github.com</span>
+            </div>
+          </a>
+          <a href="https://xbox.com">
+            <i class="fa-brands fa-xbox"></i>
+            <div class="label">
+              <span class="name">Xbox</span>
+              <span class="url">xbox.com</span>
+            </div>
+          </a>
+        </div>
+        <div id="discover-container">
+          <div id="discover-toggles">
+            <button type="button">Discover</button>
+          </div>
+          <div id="discover-feed">
+            <a href="https://news.google.com/home?hl=es-419&gl=US&ceid=US:es-419" class="discover-card">
+              <img src="https://chromeunboxed.com/wp-content/uploads/2021/09/Google-News-Feature.jpg?ezimgfmt=ngcb92/notWebPs" />
+              <div class="discover-card-info">
+                <p class="title">Google News</p>
+                <p class="desc">Google News is a news aggregator service developed by Google.</p>
+              </div>
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+</div>
+<p id='inicioxc'>.</p>
+<img className='background3' src='./texture.gif'/>
+<div className='glow3'></div>
+<div id='Services'>
+<h1 className='tituloServices' >Services</h1>
+<div class="container">
+     <div class="card">
+       <div class="face face1">
+         <div class="content">
+            <i class="fab fa-windows"></i>            
+           <img className='logoC' src='./C++.png'></img>
+         </div>
+       </div>
+       <div class="face face2">
+         <div class="content">
+           <p> C++ is a high-level general-purpose programming language created by Danish computer scientist Bjarne Stroustrup and first released in 1985 as an extension of the C programming language, or "C with Classes".</p>
+         </div>
+       </div>
+    </div>
+    
+    <div class="card">
+       <div class="face face1">
+         <div class="content">
+      <i class="fab fa-android"></i>
+      <img className='logoC' src='./java.png'></img>
+         </div>
+       </div>
+       <div class="face face2">
+         <div class="content">
+           <p>Java is a high-level, class-based, object-oriented programming language that is designed to have as few implementation dependencies as possible.</p>
+         </div>
+       </div>
+    </div>
+
+    <div class="card">
+       <div class="face face1">
+         <div class="content">
+           <i class="fab fa-apple"></i>
+           <img className='logoC' src='./html.png'></img>
+         </div>
+       </div>
+       <div class="face face2">
+         <div class="content">
+           <p>The HyperText Markup Language or HTML is the standard markup language for documents designed to be displayed in a web browser. It can be assisted by technologies such as Cascading Style Sheets and scripting languages such as JavaScript.</p>
+         </div>
+       </div>
+    </div>
+
+    <div class="card">
+       <div class="face face1">
+         <div class="content">
+           <i class="fab fa-apple"></i>
+           <img className='logoC' src='./css.png'></img>
+         </div>
+       </div>
+       <div class="face face2">
+         <div class="content">
+           <p>Cascading Style Sheets is a style sheet language used for describing the presentation of a document written in a markup language such as HTML or XML. CSS is a cornerstone technology of the World Wide Web, alongside HTML and JavaScript.</p>
+         </div>
+       </div>
+    </div>
+
+    <div class="card">
+       <div class="face face1">
+         <div class="content">
+           <i class="fab fa-apple"></i>
+           <img className='logoC' src='./javascrip.png'></img>
+         </div>
+       </div>
+       <div class="face face2">
+         <div class="content">
+           <p>JavaScript, often abbreviated as JS, is a programming language that is one of the core technologies of the World Wide Web, alongside HTML and CSS. As of 2022, 98% of websites use JavaScript on the client side for webpage behavior, often incorporating third-party libraries.</p>
+         </div>
+       </div>
+    </div>
+
+    <div class="card">
+       <div class="face face1">
+         <div class="content">
+           <i class="fab fa-apple"></i>
+           <img className='logoC' src='./node-js.png'></img>
+         </div>
+       </div>
+       <div class="face face2">
+         <div class="content">
+           <p>Node.js is a cross-platform, open-source server environment that can run on Windows, Linux, Unix, macOS, and more. Node.js is a back-end JavaScript runtime environment, runs on the V8 JavaScript Engine, and executes JavaScript code outside a web browser.</p>
+         </div>
+       </div>
+    </div>
+
+    <div class="card">
+       <div class="face face1">
+         <div class="content">
+           <i class="fab fa-apple"></i>
+           <img className='logoC' src='./MongoDB.png'></img>
+         </div>
+       </div>
+       <div class="face face2">
+         <div class="content">
+           <p>MongoDB is a source-available cross-platform document-oriented database program. Classified as a NoSQL database program, MongoDB uses JSON-like documents with optional schemas. MongoDB is developed by MongoDB Inc. and licensed under the Server Side Public License which is deemed non-free by several distributions. </p>
+         </div>
+       </div>
+    </div>
+
+    <div class="card">
+       <div class="face face1">
+         <div class="content">
+           <i class="fab fa-apple"></i>
+           <img className='logoC' src='./React.png'></img>
+         </div>
+       </div>
+       <div class="face face2">
+         <div class="content">
+           <p>React is a free and open-source front-end JavaScript library for building user interfaces based on components. It is maintained by Meta and a community of individual developers and companies.</p>
+         </div>
+       </div>
+    </div>
+
+  </div>
+</div>
+
+<p id='inicioxx'>.</p>
+<img className='background4' src='./texture.gif'/>
+<div className='glow4'></div>
+<div className='contact'>
+
+
+
+<section class="cards">
+<article class="car car--1">
+  <div class="car__img"></div>
+     <div class="car__img--hover"></div>
+  <div class="car__info">
+    <span class="car__category"> Discord</span>
+    <p className='porbruto'>Kycoon#0015</p>
+  </div>
+</article>
+  
+  
+<article class="car card--2">
+  <div class="car__img"></div>
+     <div class="car__img--hover"></div>
+   <div class="car__info">
+    <span class="car__category"> Gmail</span>
+    <p className='porbruto'>jomaval4@gmail.com</p>
+  </div>
+</article>  
+</section>
+
+
+
+</div>
+</div>/* este div es de la clase App */
+  );
+}
+export default App;
